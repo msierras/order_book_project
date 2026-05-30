d6d17549e74e0909763bb19246a8773456ccb4d6
Msierra001 <melvin.sierraic3@gmail.com>
2026-05-30 15:40:05 -0400

    Adding the includes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <unordered_map>
+#include <memory>
+#include <variant>
+#include <optional>
+#include <tuple>
+#include <format>
+
 
 
 int main()
